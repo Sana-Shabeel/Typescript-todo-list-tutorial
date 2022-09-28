d26cb0374d14171c6adab7fd2c3fd3af6bb55444
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-09-28 07:19:03 +0200

    Add uuid and its types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import { v4 as uuidV4 } from "uuid"; // 'uuidV4' is declared but its value is never read.' not every library is written in ts so it will throw an that error.  Try `npm i --save-dev @types/uuid` to get types if there are any for the library
+
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("new-task-form") as HTMLFormElement | null; // we have to specify the element
+const form = document.getElementById("new-task-form") as HTMLFormElement | null; // we have to specify the type of element
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  console.log(input.value);
+  console.log(uuidV4());
+});
